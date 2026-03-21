9ef95b0baeef593102a655673bb1e87b15087147
beeresh1234 <139480918+beeresh1234@users.noreply.github.com>
2026-03-21 16:22:04 +0530

    Set default URL for FQDN environment variable

--- info.py
+++ info.py
@@ -173,7 +173,7 @@ if "DYNO" in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "creative-laurene-theraj-9db62396.koyeb.app")
 
 # Commands
 admin_cmds = [
